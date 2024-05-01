7839c072de7d2d9e2ff92bfaab6cfa85ff2527cc
Rohit More <113591202+Empire016@users.noreply.github.com>
2024-05-01 15:59:57 +0530

    s

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const FullNameForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!firstName || !lastName) {
+      alert('Please fill out all fields.');
+      return;
+    }
+    alert(`Hello, ${firstName} ${lastName}!`);
+  };
+
+  return (
+    <div>
+    <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        type="text"
+        id="firstName"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}
+        required
+      />
+      <br />
+      <label htmlFor="lastName">Last Name:</label>
+      <input
+        type="text"
+        id="lastName"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}
+        required
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+    
+  );
+};
+
+export default FullNameForm;
